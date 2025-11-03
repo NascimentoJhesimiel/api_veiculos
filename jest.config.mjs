279355faef725dc3fa40fc4b4315f2501c963312
João Vitor Sotero Vieira Silva <avanade.jssilva@voeazul.com.br>
2025-11-02 23:44:39 -0300

    chore: comment out coverage configuration in Jest settings

--- jest.config.mjs
+++ jest.config.mjs
@@ -26,25 +26,25 @@ const config = {
 		"<rootDir>/tests/**/*.test.js"
 	],
 
-	// Coverage configuration
-	collectCoverageFrom: [
-		"src/**/*.js",
-		"!src/server.js",
-		"!src/**/*.spec.js",
-		"!src/**/*.test.js"
-	],
+	// Coverage configuration - COMENTADO PARA DESABILITAR
+	// collectCoverageFrom: [
+	//	"src/**/*.js",
+	//	"!src/server.js",
+	//	"!src/**/*.spec.js",
+	//	"!src/**/*.test.js"
+	// ],
 
-	coverageDirectory: "coverage",
-	coverageReporters: ["text", "lcov", "html"],
+	// coverageDirectory: "coverage",
+	// coverageReporters: ["text", "lcov", "html"],
 
 	// Indicates whether the coverage information should be collected while executing the test
-	collectCoverage: true,
+	// collectCoverage: true,
 
 	// An array of glob patterns indicating a set of files for which coverage information should be collected
 	// collectCoverageFrom: undefined,
 
 	// The directory where Jest should output its coverage files
-	coverageDirectory: 'coverage',
+	// coverageDirectory: 'coverage',
 
 	// An array of regexp pattern strings used to skip coverage collection
 	// coveragePathIgnorePatterns: [
@@ -52,7 +52,7 @@ const config = {
 	// ],
 
 	// Indicates which provider should be used to instrument code for coverage
-	coverageProvider: 'v8',
+	// coverageProvider: 'v8',
 
 	// A list of reporter names that Jest uses when writing coverage reports
 	// coverageReporters: [
