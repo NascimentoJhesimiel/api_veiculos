945794a41087e0f3dfff0cda7fb5aa537653af54
Jhesimiel <jhesimieljorge23@gmail.com>
2025-11-04 13:07:15 -0300

    feat: add user entity, registration logic and storage file

--- src/entities/user.js
+++ src/entities/user.js
@@ -1,5 +1,7 @@
-import { z } from 'zod';
+import { email, z } from 'zod';
 
 export const UserSchema = z.object({
-  
+  nome: z.string().min(2, "Username must have more then 3 characters"),
+  email: z.email(),
+  senha: z.number()
 }) 
